518468ba65127701ad8291cac86abb5b4b293596
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-11-25 20:48:47 +0300

    Moved data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,36 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+
+from places.models import Place
 
 
 def show_main_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    features = []
+    for place in Place.objects.all():
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.coordinate_lng, place.coordinate_lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": {
+                    "title": place.title,
+                    "description_short": place.description_short,
+                    "description_long": place.description_long,
+                    "coordinates": {
+                        "lat": place.coordinate_lat,
+                        "lng": place.coordinate_lng
+                    }
+                }
+            }
+        })
+    places = {
+        "type": "FeatureCollection",
+        "features": features,
+    }
+
+    context = {'places': places}
+
+    return render(request, 'index.html', context)
