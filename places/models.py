d2f3a1aeac2934869f920e2be1cf84780f55fb58
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-11-25 14:06:22 +0300

    Added a place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=100, help_text='Название')
+    description_short = models.CharField(max_length=1000, help_text='Короткое описание')
+    description_long = models.CharField(max_length=10000, help_text='Полное описание')
+    coordinate_lng = models.FloatField(help_text='Координаты(долгота)')
+    coordinate_lat = models.FloatField(help_text='Координаты(широта)')
+    created_at = models.DateTimeField(help_text='Дата создания')
+    updated_at = models.DateTimeField(blank=True, null=True, help_text='Дата обновления')
+    published_at = models.DateTimeField(blank=True, null=True, help_text='Дата публикации')
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
