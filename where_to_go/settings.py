55534287f4c99163a1754c8c599597cdfbcaaaf1
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-11-27 00:57:55 +0300

    Added a STATIC_ROOT path

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -34,11 +34,7 @@ DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-print('~~~~~~~~~~~~~~')
-print(ALLOWED_HOSTS)
-
 # Application definition
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -146,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
