e69e271d158afbfdd2bc73fd26c6c9587648f3be
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-11-26 18:57:36 +0300

    Added place images in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
+    readonly_fields = ('place_images',)
     model = PlaceImage
+
+    def place_images(self, place_image):
+        return format_html('<img src="{}" height="{}"', place_image.image.url, '200px')
 
 
 @admin.register(Place)
