0476d96826eb1560a728f42a7a0243f4092308bf
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-11-29 20:33:47 +0300

    Changed static_root path

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -130,8 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
