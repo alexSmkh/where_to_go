4af49c599c0ef6d4075e6541c311de8033993d17
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-01-22 08:19:00 +0300

    Settings to deploy on Railway

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(env('DB_URL'))
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
